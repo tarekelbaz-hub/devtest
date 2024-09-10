57b1bd755565324c57d51823e8e249eefc886969
Tarek EL Baz <85111566+tarekelbaz-hub@users.noreply.github.com>
2024-09-10 13:04:47 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
-public class HelloWorld {
+class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");   
-
+        System.out.println("Hello World!");
     }
 }
